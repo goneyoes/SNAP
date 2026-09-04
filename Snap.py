16f09850b1c1b08e62948d715db0d482ffb6c797
goneyoes <haskela@grace.edu>
2026-09-04 12:30:00 -0400

    Create Snap.py

---
+++ Snap.py
@@ -0,0 +1,85 @@
+class Card:
+    def __init__(self, name, cost, power, action):
+        self.name = name
+        self.cost = cost
+        self.power = power
+        self.action = action
+
+NICO= Card("Nico M",1,2, SAVE ME!!!)
+CARNAGE = Card("Carnage",2,2,)
+def Cdestroy(): 
+MAGIK = Card("Magik",3,2,"add 1 turncount")
+def Turn7(): TURNCOUNT=7
+VENOM = Card("Venom",3,3,"destroy")
+def Vdestroy(): 
+SHURI = Card("Shuri", 4,3,"double the power of the next card you play on this location")
+def doubler(): 
+STICK = Card("Stick",4,3,"add this card's power to your next card")
+def lend():
+ABS = Card("Absorbing Man",4,5,"copy the text of the last card you played")
+def Acopy():
+SSPIDERMAN = Card("SSpiderman",4,7,"Add this card's power to your lowest cost card here and use its action again, then remove this card")
+def SScopy():
+BP = Card("Black Panther",5,5,"Double this card's current power")
+def Panther2():
+NIMROD = Card("Nimrod",5,6,"When this is destroyed, add a nimrod with the same power to the other locations")
+#Card doesn't have a method???
+ZOLA = Card("Arnim Zola",6,0,"destroy 1 card, then copy it to the other locations and use its action")
+def Zclone():
+TASKMASTER = Card("Taskmaster",6,0,"This card's power is set to the power of your last played card")
+def tasks():
+#give the actions actual methods
+
+DECK = [NICO, CARNAGE, MAGIK, VENOM, SHURI, STICK, ABS, SSPIDERMAN, BP, NIMROD, ZOLA, TASKMASTER]
+L1 = []
+L2 = []
+L3 = []
+#need to make power check on cards
+TURN = 1
+TURNCOUNT = 6
+HAND = []
+
+def play(self, card, location):
+    
+
+    location_check(play.location)
+    if location_
+    
+def drawcard(self):
+    #take from DECK and give to HAND
+
+def location_check(self,name)
+    
+    
+        
+def turnstart(self,number):
+    drawcard
+    energy = TURN
+    
+    if TURN>TURNCOUNT:
+        endcon=1
+    else 
+    
+def endturn
+    
+    
+#make sure you account for stick shenanigans
+    
+def gamestart(self)
+    endcon=0
+    drawcard
+    drawcard
+    drawcard
+    TURN = 1
+    turnstart
+    del L1[:]
+    del L2[:]
+    del L3[:]
+    
+goods = 0
+while endcon=0:
+    goods+=1
+    turn = 1
+    
+    
+    
