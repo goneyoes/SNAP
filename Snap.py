9c22a7d06838b21c0f9ea52f42daf3652df2eff9
goneyoes <haskela@grace.edu>
2026-09-08 14:37:52 -0400

    Update Snap.py

--- Snap.py
+++ Snap.py
@@ -1,58 +1,78 @@
+import random
 class Card:
-    def __init__(self, name, cost, power, action):
+    def __init__(self, name, cost, power, action=None, location=DECK):
         self.name = name
         self.cost = cost
         self.power = power
         self.action = action
+        self.location = location
 
 NICO= Card("Nico M",1,2, SAVE ME!!!)
-CARNAGE = Card("Carnage",2,2,)
+
+def cc:
+def Lo:
+def move:
+def Dem:
+#she has Panther2
+def add2:
+def Nclone: 
+NicoM = []
+
+CARNAGE = Card("Carnage",2,2, Cdestroy)
 def Cdestroy(): 
-MAGIK = Card("Magik",3,2,"add 1 turncount")
+MAGIK = Card("Magik",3,2, Turn7)
 def Turn7(): TURNCOUNT=7
-VENOM = Card("Venom",3,3,"destroy")
+VENOM = Card("Venom",3,3,Vdestroy)
 def Vdestroy(): 
-SHURI = Card("Shuri", 4,3,"double the power of the next card you play on this location")
-def doubler(): 
-STICK = Card("Stick",4,3,"add this card's power to your next card")
-ABS = Card("Absorbing Man",4,5,"copy the text of the last card you played")
+SHURI = Card("Shuri", 4,3, doubler)
+def doubler(): self.card.power = self.card.power*2
+STICK = Card("Stick",4,3, lend)
+def lend():  self.card.power = self.card.power + STICK.power
+ABS = Card("Absorbing Man",4,5, Acopy)
 def Acopy(): 
-SSPIDERMAN = Card("SSpiderman",4,7,"Add this card's power to your lowest cost card here and use its action again, then remove this card")
+SSPIDERMAN = Card("SSpiderman",4,7, SScopy)#"Add this card's power to your lowest cost card here and use its action again, then remove this card")
 def SScopy():
 #these two cards should be easy once i define their calls
-BP = Card("Black Panther",5,5,"Double this card's current power")
+BP = Card("Black Panther",5,5, Panther2)
 def Panther2(): self.power = self.power*2
 NIMROD = Card("Nimrod",5,6,"When this is destroyed, add a nimrod with the same power to the other locations")
 #Card doesn't have a method???
-ZOLA = Card("Arnim Zola",6,0,"destroy 1 card, then copy it to the other locations and use its action")
-def Zclone(): 
+ZOLA = Card("Arnim Zola",6,0, Zclone)
+def Zclone(): i = random.randrange(len(self.Location))
+removed_card = cards.pop(i)
+#this needs far more coding
+    
 TASKMASTER = Card("Taskmaster",6,0,"This card's power is set to the power of your last played card")
 def tasks(): self.power = last_card.power
 #give the actions actual methods
 
 DECK = [NICO, CARNAGE, MAGIK, VENOM, SHURI, STICK, ABS, SSPIDERMAN, BP, NIMROD, ZOLA, TASKMASTER]
+HAND = []
 L1 = []
 L2 = []
 L3 = []
-#need to make power check on cards
-TURN = 1
-TURNCOUNT = 6
-HAND = []
 Playlist = []
 last_card = Playlist[-1]
+TURN = 1
+TURNCOUNT = 6
 def playstart
-def playcard(self, card, location):
-    Playlist.append(self.card): 
-    if last_card = STICK
-        self.card.power= self.card.power + last_card.power
-    self.location.append(self.card)
-# location part should require an input and failsafe
-    
-def drawcard(self):
-    #take from DECK and give to HAND
 
-def location_check(self,name)
+def playcard(self, card, location):
+    energy -= self.card.energy
+     if energy<0
+        print("energy error")
+        return
+    Playlist.append(self.card): 
+    self.location.append(self.card)
+    if len(self.location)>4
+        print("Location error")
+        return
+    if last_card = SHURI:
+    if last_card = STICK:
+    if last_card = NICO:
+    #print locations
     
+# location part should require an input and failsafe    
     
         
 def turnstart(self,number):
@@ -61,28 +81,32 @@ def turnstart(self,number):
     
     if TURN>TURNCOUNT:
         endcon=1
-    else 
-    
+        
 def endturn
     
-    
-#make sure you account for stick shenanigans
+def drawcard():
+    i = random.randrange(len(DECK))
+    HAND.append(i)
+    DECK.pop(i)
     
 def gamestart(self)
     endcon=0
-    drawcard
-    drawcard
-    drawcard
+    TURNCOUNT=6
     TURN = 1
-    turnstart
+    DECK = [NICO, CARNAGE, MAGIK, VENOM, SHURI, STICK, ABS, SSPIDERMAN, BP, NIMROD, ZOLA, TASKMASTER]
+    drawcard
+    drawcard
+    drawcard
     del L1[:]
     del L2[:]
     del L3[:]
+    turnstart
     
-goods = 0
-while endcon=0:
-    goods+=1
-    turn = 1
-    
+"""while endcon=0
+    gamestart
+"""
+
+print(Playlist)
+print #locations and powers
     
     
